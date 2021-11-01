74358db78d8310a3c5fa3cdb0f59be8c00691ce4
Eudes Junior <juniorsousa_343@hotmail.com>
2021-11-01 12:24:28 -0300

    ClimaAtual

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ document.querySelector('.busca')
     if (input !== '') {
       showWarning('Carregando');
 
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=3f72cd7d686f93ae35524a96207ad38c&units=metric&lang=pt_br`
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=3f72cd7d686f93ae35524a96207ad38c&units=metric&lang=pt_br`;
 
       let results = await fetch(url);
       let json = await results.json();
 
-      if (json.code === 200) {
+      if (json.cod === 200) {
         showInfo({
           name: json.name,
           country: json.sys.country,
@@ -37,7 +37,7 @@ function showInfo(json) {
   document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
   document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>°C</sup>`;
   document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>Km/h</span>`;
-  document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}d@2x.png`);
+  document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
   document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`;
   document.querySelector('.resultado').style.display = 'block';
 };
